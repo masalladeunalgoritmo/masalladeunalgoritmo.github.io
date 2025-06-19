a144c45f4813df19ee2b7d21ac34332581c72015
Rey Ángeles <masalladeunalgoritmo@gmail.com>
2025-06-19 10:33:19 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ let selectedImg = null;
 let selectedLine = "";
 
 function preload() {
-  for (let i = 1; i <= 7; i++) {
+  for (let i = 1; i <= 40; i++) {
     imagenes.push(loadImage(`assets/${i}.png`));
   }
 
