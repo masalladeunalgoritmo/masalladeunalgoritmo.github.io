ef7abfc55ada34f8e10b51c4684a0085b150ec67
Rey Ángeles <masalladeunalgoritmo@gmail.com>
2025-06-26 12:45:55 -0600

    Actualizar sketch.js

--- sketch.js
+++ sketch.js
@@ -8,9 +8,9 @@ let textoAnterior = "";
 let textoVisible = "";
 
 let tiempoUltimoCambio = 0;
-let intervaloCambio = 6000; // cambia cada 6 segundos
-let duracionFade = 1000;     // crossfade de 1s
-let velocidadEscritura = 40; // ms por letra
+let intervaloCambio = 6000;
+let duracionFade = 1000;
+let velocidadEscritura = 40;
 
 function preload() {
   for (let i = 1; i <= 40; i++) {
@@ -52,21 +52,31 @@ function draw() {
   let ahora = millis();
   let tiempoTranscurrido = ahora - tiempoUltimoCambio;
   let alpha = constrain(tiempoTranscurrido / duracionFade, 0, 1);
+  let glitchActivo = tiempoTranscurrido < 1000;
 
-  // Crossfade de imágenes
-  if (imagenAnterior) {
+  // Imagen anterior
+  if (imagenAnterior && glitchActivo) {
+    tint(255, 255 * (1 - alpha));
+    ghostTrailDatamosh(imagenAnterior);
+    noTint();
+  } else if (imagenAnterior) {
     tint(255, 255 * (1 - alpha));
     image(imagenAnterior, width / 2, height / 2, 1080, 1920);
     noTint();
   }
 
-  if (imagenActual) {
+  // Imagen actual
+  if (imagenActual && glitchActivo) {
+    tint(255, 255 * alpha);
+    ghostTrailDatamosh(imagenActual);
+    noTint();
+  } else if (imagenActual) {
     tint(255, 255 * alpha);
     image(imagenActual, width / 2, height / 2, 1080, 1920);
     noTint();
   }
 
-  // Máquina de escribir
+  // Máquina de escribir + shake de texto
   let letrasMostrar = floor((tiempoTranscurrido - duracionFade) / velocidadEscritura);
   if (letrasMostrar >= 0) {
     textoVisible = textoActual.substring(0, letrasMostrar);
@@ -74,8 +84,10 @@ function draw() {
     textoVisible = "";
   }
 
+  let shakeX = glitchActivo ? random(-2, 2) : 0;
+  let shakeY = glitchActivo ? random(-2, 2) : 0;
   fill(255, 255 * alpha);
-  text(textoVisible, width / 2, height / 2 + 600);
+  text(textoVisible, width / 2 + shakeX, height / 2 + 600 + shakeY);
 
   if (tiempoTranscurrido > intervaloCambio) {
     cambiarContenido(false);
@@ -99,5 +111,21 @@ function cambiarContenido(primeraVez = false) {
       imagenAnterior = null;
       textoAnterior = "";
     }
+  }
+}
+
+// Glitch horizontal tipo datamosh
+function ghostTrailDatamosh(img) {
+  let h = img.height;
+  let w = img.width;
+  let glitchHeight = 20;
+  for (let y = 0; y < h; y += glitchHeight) {
+    let offset = int(random(-30, 30));
+    copy(
+      img,
+      0, y, w, glitchHeight,
+      width / 2 - w / 2 + offset, height / 2 - h / 2 + y,
+      w, glitchHeight
+    );
   }
 }
